047c93dd624d94d3ce91617fcdd39604e4a9a543
LuisenLou97 <luisen.sb97@gmail.com>
2024-06-07 11:45:13 +0200

    change for login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "containers/pages/Home";
 import Search from "containers/pages/Search";
 import AboutUs from "containers/pages/AboutUs";
 import Communicate from "containers/pages/Communicate";
+import Sign from 'containers/pages/Sign'
 import Error404 from "containers/error/Error404";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -22,6 +23,8 @@ function App() {
             <Route path='/busqueda' element={<Search/>}/>
             <Route path='/nosotros' element={<AboutUs/>}/>
             <Route path='/contacto' element={<Communicate/>}/>
+
+            <Route path='/sesion' element={<Sign/>}/>
             
             
           </Routes>
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,11 +1,13 @@
 import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
 
 
 
 
 
 function Navbar(){
+
 
     window.onscroll = function(){scrollFunction()}
 
@@ -21,6 +23,7 @@ function Navbar(){
     }
 
 
+
     return (
         <nav id='navbar' className="w-full py-2 top-0 transition duration-300 bg-teal-700 fixed ease-in-out z-50">     
             <div className="ml-4 mt-2 mb-5 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14">
@@ -33,12 +36,14 @@ function Navbar(){
                     <NavLink to='/busqueda' className="text-lg inline-flex font-medium leading-6 text-violet-50 border-b-2 border-white hover:border-emerald-500 mx-4">Search</NavLink>
                     <NavLink to='/nosotros' className="text-lg inline-flex font-medium leading-6 text-violet-50 border-b-2 border-white hover:border-emerald-500 mx-4">About Us</NavLink>
                     <NavLink to='/contacto' className="text-lg inline-flex font-medium leading-6 text-violet-50 border-b-2 border-white hover:border-emerald-500 mx-4">Contact</NavLink>
-                    <button
-                        type="button"
-                        className="ml-6 w-full sm:w-auto relative px-8 py-4 inline-flex items-center rounded-md border sm:px-6 sm:py-6 md:px-4 md:py-2 lg:px-3 lg:py-1 border-transparent bg-emerald-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
-                    >
-                        Login / Register
-                    </button>
+                    <Link to='/sesion'>
+                        <button
+                            className="ml-6 w-full sm:w-auto relative px-8 py-4 inline-flex items-center rounded-md border sm:px-6 sm:py-6 md:px-4 md:py-2 lg:px-3 lg:py-1 border-transparent bg-emerald-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+                        >
+                            Login / Register
+                        </button>
+                    </Link>
+                    
                 </div>
             </div>
         </nav>
