9682c2bbf3995a1c7b7b9656824f88cfc32ff247
LuisenLou97 <luisen.sb97@gmail.com>
2024-06-09 21:30:16 +0200

    added incidents

--- apps/incident/models.py
+++ apps/incident/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from apps.account.models import Account
+from apps.flat.models import Flat 
 
 # Create your models here.
+
+class Incident(models.Model):
+    class Meta:
+        verbose_name = 'Incident'
+        verbose_name_plural = 'Incidents'
+
+
+    flat = models.ForeignKey(Flat, on_delete = models.CASCADE)
+    applicant = models.ForeignKey (Account, on_delete= models.CASCADE)
+    receiver = models.ForeignKey (Account, on_delete=models.CASCADE)
+
+    STATUS_CHOICES = (
+        ('Aceptado', 'Accepted'),
+        ('Rechazada', 'Rejected'),
+        ('Pendiente', 'Pending'),
+    )
+
+    incident_status = models.CharField (max_length = 3, choices = STATUS_CHOICES)
+    incident_date = models.DateField(auto_now_add = True)
+
--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
 
     path('api/flat/', include('apps.flat.urls')),
     path('api/room/', include('apps.room.urls')),
+    path('api/account/', include('apps.account.urls')),
+    path('api/address/', include('apps.address.urls')),
+    path('api/incident/', include('apps.incident.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 
 urlpatterns += [re_path(r'^.*',TemplateView.as_view(template_name='index.html'))]
