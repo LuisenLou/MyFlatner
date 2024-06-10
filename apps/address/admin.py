17aaf534825e51adb1be50a261b46116af66b86f
LuisenLou97 <luisen.sb97@gmail.com>
2024-06-10 19:58:17 +0200

    fixed

--- apps/address/admin.py
+++ apps/address/admin.py
@@ -4,10 +4,10 @@ from .models import *
 # Register your models here.
 
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ('id', 'via_class', 'via_name', 'via_number', 'floor', 'door', 'postal_code', 'city', 'state', 'country')
+    list_display = ('id', 'via_class', 'via_name', 'via_number', 'floor', 'door', 'postal_code', 'city', 'province_or_state', 'country')
     list_display_links = ('id', 'via_name')
-    list_filter = ('via_class', 'city', 'state', 'country')
-    search_fields = ('via_name', 'city', 'state', 'country')
+    list_filter = ('via_class', 'city', 'province_or_state', 'country')
+    search_fields = ('via_name', 'city', 'province_or_state', 'country')
     ordering = ('id',)
     list_per_page = 25
 
